6c1901bdfaf4455e1c6c2fc6d52cba7544e06ae9
Your Name <you@example.com>
2024-10-16 17:52:51 -0500

    added transaction class

--- src/main/java/com/ps/Transaction.java
+++ src/main/java/com/ps/Transaction.java
@@ -1,4 +1,32 @@
 package com.ps;
 
+import java. time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    // Constructor
+    public Transaction(String description, String vendor, double amount) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+
+        this.date = now.format(dateFormatter);
+        this.date = now.format(dateFormatter);
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    // Getter method for formatting transaction details for the CSV file
+    public String getTransactionDetails() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
 }
